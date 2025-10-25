9b3454dec57b71c54ece915e7a4ab50d70f70df7
Capslock66 <mail.thierry.parent@gmail.com>
2025-10-25 17:23:15 +0200

    Add minimal led blink

--- Clock/src/main.cpp
+++ Clock/src/main.cpp
@@ -1,10 +1,23 @@
 #include <Arduino.h>
 
+#define LED_PIN 8  // LED intégrée du SuperMini
+
 
 void setup() {
-  // put your setup code here, to run once:
+
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(115200);
+  delay(5000);
+  Serial.println("Start clock...");
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    digitalWrite(LED_PIN, HIGH);
+    delay(300);
+
+    digitalWrite(LED_PIN, LOW);
+    delay(300);
+    Serial.println("loop clock");
 }
